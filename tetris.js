978a72d0e57f3198d386eab71a5846114375f4b3
TheWillBa <wbabincsak@gmail.com>
2020-10-16 10:18:59 -0400

    Added drawing the world to the canvas

--- tetris.js
+++ tetris.js
@@ -2,39 +2,10 @@
 // Written using function programming concepts,
 // design is from version previously written in LISP-style language Racket
 
-//#region test pow function
-function pow(x, n){
-    if (n < 0) return NaN;
-    if (Math.round(n) != n) return NaN;
-  
-    let result = 1;
-  
-    for (let i = 0; i < n; i++) {
-      result *= x;
-    }
-  
-    return result;
-  }
-//#endregion
-//#region Pow test cases 
 
-
-
-  describe("pow", function() {
-
-    it("2 raised to power 3 is 8", function() {
-      chai.assert.equal(pow(2, 3), 8);
-    });
-  
-    it("3 raised to power 4 is 81", function() {
-      chai.assert.equal(pow(3, 4), 81);
-    });
-  
-  });
-//#endregion
 
 const gridX = 10;
-const gridY = 10;
+const gridY = 20;
 
 const xOffset = 50;
 const yOffset = 50;
@@ -57,6 +28,9 @@ class Block {
   }
 }
 
+let b1 = new Block(50, 50, "red", 10);
+let b2 = new Block(200, 100, "purple", 10);
+
 class TetrinoTemplate {
   constructor(locations, pX, pY, color) {
     this.locations = locations,
@@ -73,3 +47,90 @@ class Tetrino {
       this.pY = pY;
   }
 }
+
+
+class WorldState {
+  constructor(activeTetrino, fallenBlocks, fallTimer, currentTime, points) {
+      this.activeTetrino = activeTetrino,
+      this.fallenBlocks = fallenBlocks,
+      this.fallTimer = fallTimer,
+      this.currentTime = currentTime,
+      this.points = points
+  }
+}
+
+
+// Entry into the game
+
+let blocks1 = [new Block(blockSize*5, blockSize*5, "purple", blockSize),
+                new Block(blockSize*6, blockSize*5, "purple", blockSize),
+                new Block(blockSize*7, blockSize*5, "purple", blockSize),
+                new Block(blockSize*6, blockSize*4, "purple", blockSize)];
+
+let t1 = new Tetrino(blocks1, 65, 40);
+
+let WORLD = new WorldState(t1, [], 10, 10, 0);
+// Use and modify a global world variable to assure that input modifications are not missed
+let playing = true;
+
+function play(){
+  while(playing){
+    WORLD = nextWorldState(WORLD)
+    drawWorldState(WORLD)
+    
+    // handle use input?
+  }
+}
+
+// Handle drawing
+
+const canvas = document.getElementById("canvas");
+canvas.width = xOffset*2 + gridWidthRaw;
+canvas.height = yOffset*2 + gridHeightRaw;
+const ctx = canvas.getContext("2d");
+
+
+// Draws a block on the canvas, impure function
+
+function drawBlock(b){
+  ctx.beginPath();
+  ctx.fillStyle = b.color;
+  ctx.rect(b.x, b.y, b.size, b.size);
+  ctx.fill();  
+}
+//drawBlock(b1);
+//drawBlock(b2);
+
+// Draws a tetrino on the canvas, impure function
+// Essentially wraps drawlob, necessary?
+
+function drawTetrino(t){
+  drawListOfBlocks(t.blocks);
+}
+//drawTetrino(t1);
+
+function drawListOfBlocks(lob){
+  for(b of lob){
+    drawBlock(b);
+  }
+}
+
+// Draws the worldstate, so it handles the grid and the blocks
+
+function drawWorldState(ws){
+  let blocks = ws.fallenBlocks.concat(ws.activeTetrino.blocks);
+  drawListOfBlocks(blocks);
+  drawGrid();
+}
+
+function drawGrid(){
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.rect(xOffset, yOffset, gridWidthRaw, gridHeightRaw);
+  ctx.stroke();  
+}
+
+drawWorldState(WORLD);
+
+
+
